be40967736f40c4e84390ffc16bcd06536b1639f
ali <aliyasarr97@gmail.com>
2022-05-01 00:09:01 +0300

    package count thread integrated

--- udp_forward.c
+++ udp_forward.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main()
+int count = 0;
+
+/*This thread shows incoming package count
+  on every one minute*/
+void *package_count(void *data)
 {
+    while(1)
+    {
+        count++;
+        sleep(1);
+        printf("count=%d \n", count);
+
+    }
+    return NULL;
+}
+
+int main(void) {
+    pthread_t th1;
+    pthread_create(&th1, NULL, package_count, NULL);
+    
+    while(1)
+        ;
+
+    printf("Exiting from main program\n");
+    return 0;
 }
